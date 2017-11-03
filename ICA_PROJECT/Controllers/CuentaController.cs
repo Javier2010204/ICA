255ba4c439243b44417228554f5291efdb2c9c0e
Javier Santa Cruz <2010204javiergomez@gmail.com>
2017-11-03 00:44:51 -0600

    first

---
+++ ICA_PROJECT/Controllers/CuentaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ICA_PROJECT.Controllers
+{
+    public class CuentaController : Controller
+    {
+        // GET: Cuenta
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
---
+++ ICA_PROJECT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ICA_PROJECT.MvcApplication" Language="C#" %>
